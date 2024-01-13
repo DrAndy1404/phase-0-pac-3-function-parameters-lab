9f0c01a197bded33d8a9fb6ed942be2bd1b331b7
Andrew <andrew.dimou10@gmail.com>
2024-01-13 15:07:36 +1100

    Initialize git

---
+++ index.js
@@ -0,0 +1,12 @@
+// function sayHelloTo(firstName = `Andrew`) {
+//     console.log(`Hello, ${firstName}!`);
+//   }
+
+//   sayHelloTo()
+
+
+  function introductionWithLanguageOptional(name = `Andrew`, language = `JavaScript`)  {
+    console.log(`Hi, my name is ${name}, and im learning ${language}.`);
+  }
+
+introductionWithLanguageOptional()
